6cecfe934788669e1cb4c1d89d868849ae38837e
Joe Chen <joejoevictor@gmail.com>
2016-04-24 20:59:02 -0700

    Initial commit

---
+++ src/examples/math_expression_evaluator.cpp
@@ -0,0 +1,7 @@
+
+#include "math_expression_evaluator.h"
+
+double math_expression_evaluator::evaluate(std::string expr)
+{
+    return 0.0;
+}
---
+++ src/examples/math_expression_evaluator.h
@@ -0,0 +1,15 @@
+
+#ifndef JAVA_COMPILER_MATH_EXPRESSION_EVALUATOR_H
+#define JAVA_COMPILER_MATH_EXPRESSION_EVALUATOR_H
+
+#include <string>
+#include <stddef.h>
+
+class math_expression_evaluator
+{
+public:
+    double evaluate(std::string expr);
+};
+
+
+#endif //JAVA_COMPILER_MATH_EXPRESSION_EVALUATOR_H
